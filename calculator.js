d8e84f9059de96c3cd6bbc9185d150542d8fba00
Miyukiin <jr.concha99@gmail.com>
2025-06-10 09:55:26 +0800

    Add single-expression functional calculator
    Implement logic that evaluates single-expression correctly and updates
    screenDisplay by appending the result to the expression. (9*9) => (9*9 = 81).

--- calculator.js
+++ calculator.js
@@ -3,18 +3,47 @@ let operandOne = null;
 let operatorSymbol = null;
 let operandTwo = null;
 
-const buttonsArray = Array.from(document.querySelectorAll("button.calculator-button")).filter((item)=> item.value !== "=");
-const screenDisplay = document.querySelector("div.calculator-screen p");
-const clearButton = document.querySelector("button#calculator-clear-button");
 
+const orderOfOperationsMap = {
+    "*": 1,
+    "/": 2,
+    "+": 3,
+    "-": 4,
+}
+const operatorsString = "*/+-";
+
+let expressionString = null;
+let expressionStack = [];
+let resultFlag = false;
+let result = null;
+
+const screenDisplay = document.querySelector("div.calculator-screen p");
+
+const equalsButton = Array.from(document.querySelectorAll("button.calculator-button")).filter((item)=> item.value == "=");
+equalsButton[0].addEventListener("click", () => {
+    preProcessExpression(screenDisplay.textContent);
+    result = operate(parseInt(operandOne), operatorSymbol, parseInt(operandTwo));
+    if(result){
+        resultFlag = true;
+    }
+    else{
+        return "ERROR: CANNOT OBTAIN RESULT."
+    }
+    updateCalculatorScreen(result);
+});
+
+const clearButton = document.querySelector("button#calculator-clear-button");
 clearButton.addEventListener("click", () => clearCalculatorScreen());
 
-
+const buttonsArray = Array.from(document.querySelectorAll("button.calculator-button")).filter((item)=> item.value !== "=");
 buttonsArray.map((item) => {
     item.addEventListener("click", (e) => {
         updateCalculatorScreen(e.currentTarget.value);
     })
 })
+
+
+// Functions 
 
 function add(a,b){
     return a+b;
@@ -32,7 +61,7 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(operatorSymbol, operandOne, operandTwo){
+function operate(operandOne, operatorSymbol, operandTwo){
     switch(operatorSymbol){
         case("+"):
             return add(operandOne, operandTwo)
@@ -49,8 +78,13 @@ function operate(operatorSymbol, operandOne, operandTwo){
 
 function updateCalculatorScreen(textValue){
     let text = textValue
-    if(!typeof textValue === "number"){
+
+    if(!typeof textValue === "number" && resultFlag == false){
         text = parseInt(textValue);
+    }
+    else if(resultFlag==true){
+        text = " = " + result
+        resetVariables();
     }
 
     if(screenDisplay.textContent === "0.0"){
@@ -62,6 +96,42 @@ function updateCalculatorScreen(textValue){
 
 }
 
+function resetVariables(){
+    operandOne = null;
+    operandTwo = null;
+    operatorSymbol = null;
+    expressionString = null;
+    result = null;
+    resultFlag = false;
+}
+
 function clearCalculatorScreen(){
-    screenDisplay.textContent = "0.0"
+    screenDisplay.textContent = "0.0";
+    resetVariables();
+}
+
+function preProcessExpression(string){
+    let arrayOfCharacters = string.split("")
+    console.log(arrayOfCharacters);
+
+    for(let i=0;i<arrayOfCharacters.length;i++){
+        if(!operatorsString.includes(arrayOfCharacters[i]) && operatorSymbol === null){
+            if (operandOne == null){
+                operandOne = arrayOfCharacters[i];
+                continue;
+            }
+            operandOne += arrayOfCharacters[i];
+
+        }
+        else if (!operatorsString.includes(arrayOfCharacters[i]) && operatorSymbol !== null){
+            if (operandTwo == null){
+                operandTwo = arrayOfCharacters[i];
+                continue;
+            }
+            operandTwo += arrayOfCharacters[i];
+        }
+        else{
+            operatorSymbol = arrayOfCharacters[i];
+        }
+    }
 }
